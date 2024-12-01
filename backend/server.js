daf3f22dd061a45ad77671e7da223679fbdc2176
PratikSalam19 <pratiksalam78@gmail.com>
2024-12-02 04:22:02 +0530

    Add Header component

--- backend/server.js
+++ backend/server.js
@@ -20,3 +20,29 @@ app.use('/api/timetable', timetableRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use((err, req, res, next) => {
+  logger.error(err);
+  res.status(500).send('Internal Server Error');
+});
